ea3b038f53dcb5b7502c080de2bbbe826bcb62c7
Priyanshu jain <priyanshjain0121@gmail.com>
2021-09-09 15:48:56 +0530

    first commit

---
+++ src/main/java/git_jenkins_docker_demo/Project_3_Maven/App.java
@@ -0,0 +1,13 @@
+package git_jenkins_docker_demo.Project_3_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
